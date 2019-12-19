576840c87c43315ed81f5448ac1239d3d98f3089
Duc Bui <ducbui@umich.edu>
2019-12-19 12:40:25 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Chrome Extension for WebAnno Shortcuts
---
+++ scripts/content.js
@@ -0,0 +1,20 @@
+// Add a shortcut in the context of the WebAnno annotation web page.
+function addShortcuts() {
+    if (typeof shortcut !== 'undefined') {
+        (function init_setting() {
+            shortcut.add("Shift+d",function() {
+                // ida: setting, id4e: delete
+                document.getElementById('id4e').click();
+            },{
+            'disable_in_input':false,
+            'type':'keydown',
+            'propagate':false,
+            'target':document
+            });
+        })();
+    }
+}
+
+var script = document.createElement('script');
+script.appendChild(document.createTextNode('(' + addShortcuts + ')()'));
+document.body.appendChild(script);
